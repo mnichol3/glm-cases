208f6593398eda037b329f424e471cf649086f79
mnichol3 <nicholson.matt94@gmail.com>
2019-06-18 15:33:23 -0400

    the glm grid is stupid

--- src/glm_utils.py
+++ src/glm_utils.py
@@ -4,16 +4,15 @@ import pyproj
 import numpy as np
 from math import pi
 import matplotlib.pyplot as plt
+from sys import exit
+
+from grib import trunc
 
 def trim_header(abs_path):
     if (not isfile(abs_path)):
         raise OSError('File does not exist:', abs_path)
 
-    out_path = abs_path + '.nc'
-
-    print(out_path)
-
-    if (not isfile(out_path)):
+    if (not isfile(abs_path + '.nc') and abs_path[-3:] != '.nc'):
 
         with open(abs_path, 'rb') as f_in:
             f_in.seek(21)
@@ -26,7 +25,30 @@ def trim_header(abs_path):
             f_out.close()
             f_out = None
 
-    return out_path
+    if (abs_path[-3:] != '.nc'):
+        abs_path = abs_path + '.nc'
+
+    return abs_path
+
+
+
+def get_axis_range(coords):
+    maxs = []
+    mins = []
+    for x in coords:
+        maxs.append(max(x))
+        mins.append(min(x))
+
+    while (1e+30 in maxs):
+        maxs.remove(1e+30)
+
+    while (1e+30 in mins):
+        mins.remove(1e+30)
+
+    abs_min = min(mins)
+    abs_max = max(maxs)
+
+    return (abs_min, abs_max)
 
 
 
@@ -133,7 +155,7 @@ def read_file(abs_path, meta=False):
 
 
 
-def georeference(x, y, data, sat_lon, sat_height, sat_sweep):
+def georeference(x, y, sat_lon, sat_height, sat_sweep):
     """
     Calculates the longitude and latitude coordinates of each data point
 
@@ -163,21 +185,83 @@ def georeference(x, y, data, sat_lon, sat_height, sat_sweep):
     lons, lats = np.meshgrid(Xs, Ys)
     lons, lats = p(lons, lats, inverse=True)
 
-    lats[np.isnan(data)] = 57
-    lons[np.isnan(data)] = -152
-
     return (lons, lats)
 
 
 
-fname = '/media/mnichol3/pmeyers1/MattNicholson/glm/glm20190523/IXTR99_KNES_232122_14654.2019052322'
-print_variable(fname, 'x')
+def idx_of_nearest(coords, val):
+    X = np.abs(coords-val)
+    idx = np.where(X == X.min())
+    return (idx, coords[idx][0])
 
-"""
-meta = read_file(fname, meta=True)
-lons, lats = georeference(meta['x'], meta['y'], meta['data'], meta['lon_0'], meta['height'], meta['sweep_ang_axis'])
-plt.figure(figsize=[15,12])
-#plt.pcolormesh(meta['x'], meta['y'], meta['data']) # Works in geos projection
-plt.pcolormesh(lons, lats, meta['data'])
-plt.show()
-"""
+
+
+def create_bbox(lats, lons, point1, point2):
+    bbox = {}
+
+    p_lons = np.array([point1[1], point2[1]])
+    p_lats = np.array([point1[0], point2[0]])
+
+    lon1 = idx_of_nearest(lons, p_lons[0])
+    lon2 = idx_of_nearest(lons, p_lons[1])
+
+    lat1 = idx_of_nearest(lats, p_lats[0])
+    lat2 = idx_of_nearest(lats, p_lats[1])
+
+    if (lon1[1] > lon2[1]):
+        bbox['max_lon'] = lon1
+        bbox['min_lon'] = lon2
+    else:
+        bbox['max_lon'] = lon2
+        bbox['min_lon'] = lon1
+
+    if (lat1[1] > lat2[1]):
+        bbox['max_lat'] = lat1
+        bbox['min_lat'] = lat2
+    else:
+        bbox['max_lat'] = lat2
+        bbox['min_lat'] = lat1
+
+    return bbox
+
+
+
+def main():
+    fname = '/media/mnichol3/pmeyers1/MattNicholson/glm/glm20190523/IXTR99_KNES_232122_14654.2019052322'
+    #fname = '/media/mnichol3/pmeyers1/MattNicholson/abi/ABI-L2-CMIPF_2019_143_21_OR_ABI-L2-CMIPF-M6C06_G16_s20191432130383_e20191432140097_c20191432140168.nc'
+
+    meta = read_file(fname, meta=True)
+
+    lons, lats = georeference(meta['x'], meta['y'], meta['lon_0'], meta['height'], meta['sweep_ang_axis'])
+
+    #lats[lats == 1e+30] = np.nan
+    #lons[lons == 1e+30] = np.nan
+
+    #lons = trunc(lons, decs=3)
+    #lats = trunc(lats, decs=3)
+
+    point1 = (37.195, -102.185)
+    point2 = (34.565, -99.865)
+
+    """
+    bbox = create_bbox(lats, lons, point1, point2)
+    print(bbox)
+    print(lons.shape)
+    lons_sub = lons[890:1403, 560:570]
+    lats_sub = lats[509:554, 263:1847]
+    """
+
+
+    plt.figure(figsize=[15,12])
+    plt.axis([-102.185, -99.865, 34.656, 37.195])
+    mesh = plt.pcolormesh(lons, lats, meta['data'])
+    plt.gca().set_aspect('equal', adjustable='box')
+    #plt.scatter(lons.flatten(), lats.flatten(), c='r')
+    plt.show()
+
+
+
+
+
+if (__name__ == '__main__'):
+    main()
