154ba1a6ef5453257f2ff531780c1f4bd06a5d0a
mnichol3 <nicholson.matt94@gmail.com>
2019-06-21 12:44:29 -0400

    Added documentation

--- src/proj_utils.py
+++ src/proj_utils.py
@@ -1,11 +1,42 @@
 """
-https://www.goes-r.gov/users/docs/PUG-L1b-vol3.pdf
-pg. 56-60
+21 June 2019
+Author : Matt Nicholson
+
+This file contains functions to convert GOES-16 N/S Elevation angle and E/W
+Scanning Angle coordinates to GRS80 Ellipsoid latitude and longitude coordinates
+and vice versa.
+
+Equations obtained from:
+
+United States, Congress, National Oceanic & Atmospheric Administration.
+    “GOES R Series Product Definition and User's Guide.”
+    GOES R Series Product Definition and User's Guide, 2nd ed., vol. 3,
+    Harris Corporation, 2018, pp. 56–60. 
 """
 
 from math import degrees, radians, atan, sin, cos, sqrt, tan
 
 def scan_to_geod(y, x):
+    """
+    Calculates the GRS80 ellipsoid geodetic latitude & longitude coordinates
+    from GOES-R ABI fixed grid y & x coordinates
+
+    Parameters
+    ----------
+    y : float
+        GOES-R ABI fixed grid y coordinate representing the N/S Elevation Angle,
+        in radians
+    x : float
+        GOES-R ABI fixed grid x coordinate representing the E/W Elevation Angle,
+        in radians
+
+    Returns
+    -------
+    tuple of floats
+        Tuple containing two floats; the first is the Geodetic Latitude (in deg.),
+        the second is the Geodetic Longitude (in deg.)
+        Format: (lat, lon)
+    """
     r_eq = 6378137          # semi major axis of projection, m
     inv_f = 298.257222096   # inverse flattening
     r_pol = 6356752.31414   # semi minor axis of projection, m
@@ -33,6 +64,23 @@ def scan_to_geod(y, x):
 
 
 def geod_to_scan(lat, lon):
+    """
+    Calculates GOES-R ABI fixed grid y & x coordinates from latitude and longitude
+    coordinates
+
+    Parameters
+    ----------
+    lat : float
+        GRS80 Ellipsoid geodetic latitude coordinate, in degrees
+    lon : float
+        GRS80 Ellipsoid geodetic longitude coordinate, in degrees
+
+    Returns
+    -------
+    tuple of floats
+        Tuple of two floats; the first is the N/S Elevation angle (in radians),
+        the second is the E/W Elevation angle (in radians)
+    """
     r_eq = 6378137          # semi major axis of projection, m
     inv_f = 298.257222096   # inverse flattening
     r_pol = 6356752.31414   # semi minor axis of projection, m
@@ -59,6 +107,27 @@ def geod_to_scan(lat, lon):
 
 
 def _calc_a(x, y, r_eq, r_pol):
+    """
+    Calculates the a coefficient used to calculate the distance from the satellite
+    to a point P
+
+    Parameters
+    ----------
+    x : float
+        GOES-R ABI fixed grid y coordinate representing the N/S Elevation Angle,
+        in radians
+    y : float
+        GOES-R ABI fixed grid y coordinate representing the N/S Elevation Angle,
+        in radians
+    r_eq : int
+        GOES-16 semi-major axis of projection, in meters
+    r_pol
+        GOES-16 semi-minor axis of projection, in meters
+
+    Returns
+    -------
+    float
+    """
     f = sin(x)**2 + cos(x)**2
     g = cos(y)**2 + (r_eq**2 / r_pol**2) * (sin(y)**2)
     return f * g
@@ -66,17 +135,69 @@ def _calc_a(x, y, r_eq, r_pol):
 
 
 def _calc_b(x, y, H):
+    """
+    Calculates the b coefficient used to calculate the distance from the satellite
+    to a point P
+
+    Parameters
+    ----------
+    x : float
+        GOES-R ABI fixed grid y coordinate representing the N/S Elevation Angle,
+        in radians
+    y : float
+        GOES-R ABI fixed grid y coordinate representing the N/S Elevation Angle,
+        in radians
+    H : int
+        GOES-16 projection perspective point height, in meters
+
+    Returns
+    -------
+    float
+    """
     f = -2 * H * (cos(x)**2) * (cos(y)**2)
     return f
 
 
 
 def _calc_c(H, r_eq):
+    """
+    Calculates the b coefficient used to calculate the distance from the satellite
+    to a point P
+
+    Parameters
+    ----------
+    H : int
+        GOES-16 projection perspective point height, in meters
+    r_eq : int
+        GOES-16 semi-major axis of projection, in meters
+
+    Returns
+    -------
+    float
+    """
     return (H**2 - r_eq**2)
 
 
 
 def _calc_rs(a, b, c):
+    """
+    Calculates the distance from the satellite to a point P using the quadratic
+    formula
+
+    Parameters
+    ----------
+    a : float
+        'a' coefficient for the quadratic formula
+    b : float
+        b coefficient for the quadratic formula
+    c : float
+        c coefficient for the quadratic formula
+
+    Returns
+    -------
+    float
+        distance of the satellite from point P, in meters
+    """
     num = -b - sqrt((b**2) - 4*a*c)
     den = 2 * a
     return num / den
@@ -84,23 +205,94 @@ def _calc_rs(a, b, c):
 
 
 def _calc_sx(r_s, x, y):
+    """
+    Calculates the x-axis of the satellite coordinate frame for scan_to_geod
+
+    Parameters
+    ----------
+    r_s : float
+        Distance from the satellite to point P, in meters
+    x : float
+        GOES-R ABI fixed grid x coordinate representing the E/W Elevation Angle,
+        in radians
+    y : float
+        GOES-R ABI fixed grid y coordinate representing the N/S Elevation Angle,
+        in radians
+
+    Returns
+    -------
+    s_x : float
+        x-axis of the satellite coordinate frame
+    """
     s_x = r_s * cos(x) * cos(y)
     return s_x
 
 
 
 def _calc_sy(r_s, x):
+    """
+    Calculates the y-axis of the satellite coordinate frame for scan_to_geod
+
+    Parameters
+    ----------
+    r_s : float
+        Distance from the satellite to point P, in meters
+    x : float
+        GOES-R ABI fixed grid x coordinate representing the E/W Elevation Angle,
+        in radians
+
+    Returns
+    -------
+    s_y : float
+        y-axis of the satellite coordinate frame
+    """
     s_y = -r_s * sin(x)
     return s_y
 
 
 
 def _calc_sz(r_s, x, y):
+    """
+    Calculates the z-axis of the satellite coordinate frame for scan_to_geod
+
+    Parameters
+    ----------
+    r_s : float
+        Distance from the satellite to point P, in meters
+    x : float
+        GOES-R ABI fixed grid x coordinate representing the E/W Elevation Angle,
+        in radians
+    y : float
+        GOES-R ABI fixed grid y coordinate representing the N/S Elevation Angle,
+        in radians
+
+    Returns
+    -------
+    s_z : float
+        z-axis of the satellite coordinate frame
+    """
     s_z = r_s * cos(x) * sin(y)
 
 
 
 def _calc_thetac(r_eq, r_pol, lat):
+    """
+    Calculates the geocentric latitude, in radians
+
+    Parameters
+    ----------
+    r_eq : int
+        GOES-16 semi-major axis of projection, in meters
+    r_pol : int
+        GOES-16 semi-minor axis of projection, in meters
+    lat : float
+        GRS80 geodetic latitude, in radians
+
+    Returns
+    -------
+    theta_c : float
+        Geocentric latitude, in radians
+    """
     theta_c = (r_pol**2) / (r_eq**2)
     theta_c = theta_c * tan(lat)
     theta_c = atan(theta_c)
@@ -109,6 +301,23 @@ def _calc_thetac(r_eq, r_pol, lat):
 
 
 def _calc_rc(r_pol, e, theta_c):
+    """
+    Calculates the geodetic distance to the point on the ellipsoid
+
+    Parameters
+    ----------
+    r_pol : int
+        GOES-16 semi-minor axis of projection, in meters
+    e : float
+        Uh...
+    theta_c
+        Geocentric latitude, in radians
+
+    Returns
+    -------
+    r_c : float
+        Geocentric distance to the point on the ellipsoid, in meters
+    """
     den = sqrt(1 - e**2 * cos(theta_c)**2)
     r_c = r_pol / den
     return r_c
@@ -116,17 +325,72 @@ def _calc_rc(r_pol, e, theta_c):
 
 
 def _calc_sx_inv(H, r_c, theta_c, lon, lambda_0):
+    """
+    Calculates the x-axis of the satellite coordinate frame for geod_to_scan
+
+    Parameters
+    ----------
+    H : int
+        GOES-16 imagery projection perspective point height + semi-major axis, in meters
+    r_c : float
+        Geodetic distance to the point on the ellipsoid
+    theta_c : float
+        Geocentric latitude, in radians
+    lon : float
+        Geodetic longitude coordinate, in radians
+    lambda_0: float
+        GOES-16 longitude of projection origin, in radians
+
+    Returns
+    -------
+    s_x : float
+        X-axis of the satellite coordinate frame
+    """
     s_x = H - (r_c * cos(theta_c) * cos(lon - lambda_0))
     return s_x
 
 
 
 def _calc_sy_inv(r_c, theta_c, lon, lambda_0):
+    """
+    Calculates the y-axis of the satellite coordinate frame for geod_to_scan
+
+    Parameters
+    ----------
+    r_c : float
+        Geodetic distance to the point on the ellipsoid
+    theta_c : float
+        Geocentric latitude, in radians
+    lon : float
+        Geodetic longitude coordinate, in radians
+    lambda_0: float
+        GOES-16 longitude of projection origin, in radians
+
+    Returns
+    -------
+    s_y : float
+        Y-axis of the satellite coordinate frame
+    """
     s_y = -r_c * cos(theta_c) * sin(lon - lambda_0)
     return s_y
 
 
 
 def _calc_sz_inv(r_c, theta_c):
+    """
+    Calculates the z-axis of the satellite coordinate frame for geod_to_scan
+
+    Parameters
+    ----------
+    r_c : float
+        Geodetic distance to the point on the ellipsoid
+    theta_c : float
+        Geocentric latitude, in radians
+
+    Returns
+    -------
+    s_z : float
+        Z-axis of the satellite coordinate frame
+    """
     s_z = r_c * sin(theta_c)
     return s_z
