efe9917f9252248dbb775f829ac6fd40b1330dc3
mnichol3 <nicholson.matt94@gmail.com>
2019-06-25 16:47:20 -0400

    Working dual plotting func

--- src/main.py
+++ src/main.py
@@ -10,12 +10,27 @@ import goesawsinterface
 import grib
 import localglminterface
 import glm_utils
+import plotting_funcs
+
+
 
 def main():
     local_abi_path = '/media/mnichol3/pmeyers1/MattNicholson/goes'
     local_wtlma_path = '/media/mnichol3/pmeyers1/MattNicholson/wtlma'
     local_glm_path = '/media/mnichol3/pmeyers1/MattNicholson/glm'
 
+    abs_path_glm = '/media/mnichol3/pmeyers1/MattNicholson/glm/glm20190523/IXTR99_KNES_232121_40312.2019052321'
+    abs_path_wtlma = '/media/mnichol3/pmeyers1/MattNicholson/wtlma/2019/05/23/LYLOUT_190523_212000_0600.dat'
+
+    point1 = (37.195, -102.185)
+    point2 = (34.565, -99.865)
+
+    wtlma_df = wtlma.parse_file(abs_path_wtlma)
+    glm_data = glm_utils.read_file(abs_path_glm, meta=True)
+
+    plotting_funcs.plot_mercator_dual(glm_data, (point1, point2), wtlma_df.data)
+
+    """
     conn = goesawsinterface.GoesAWSInterface()
     imgs = conn.get_avail_images_in_range('goes16', 'ABI-L2-CMIPM', '5-23-2019-20:00', '5-23-2019-21:00', 'M1', '13')
     for x in imgs:
@@ -24,6 +39,7 @@ def main():
     avail_glm_imgs = localglminterface.get_files_in_range('/media/mnichol3/pmeyers1/MattNicholson/glm/glm20190523', '5-23-2019-20:00', '5-23-2019-21:00')
     for x in avail_glm_imgs:
         print(x)
+    """
 
 
 
